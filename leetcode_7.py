7a6863b18af1d8ad838f2429209b360551cd7028
Aabdulloh2005 <ganievabdulloh2005@gmail.com>
2024-01-12 17:27:04 +0500

    Leetcode 7-problem

--- leetcode_7.py
+++ leetcode_7.py
@@ -1,10 +1,7 @@
-def reverse(x: int) -> int:
-    a = str(x)[::-1]
-    if x < 0:
-        a = "-" + a[:-1]
-    
-    
-    return int(a)
-    
-
-print(reverse(int(input())))
+class Solution:
+    def reverse(self, x: int) -> int:
+        a = str(x)[::-1]
+        if x < 0:
+            a = "-" + a[:-1]
+        a = int(a)
+        return a if -2147483648<a<2147483647 else 0
